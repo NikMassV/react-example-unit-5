b4fcd346cfc31ab589238f918a1593dfe352085b
NikMassV <slipknoshka777@gmail.com>
2024-02-12 09:14:59 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import {useState} from 'react';
 
 function App() {
+
+  let textInput = React.createRef();
+  let textOut = React.createRef();
+  const [output, setOutput] = useState('hello');
+
+  function f1(arg) {
+    console.log('f1 work ' + arg);
+  }
+
+  function showInput(event) {
+    console.log('input');
+    // console.log(event.target.value);
+    console.log(textInput.current.value);
+    textOut.current.innerHTML = textInput.current.value;
+    setOutput(textInput.current.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Events</h1>
+      <section>
+        <h2>Button</h2>
+        <button onClick={()=>f1(1)}>Push</button>
+      </section>
+      <section>
+        <h2>Input</h2>
+        {/* <div className="test" onDoubleClick = {()=>f1(2)} onMouseMove = {f2}></div> */}
+        <input type="text" onInput={showInput} ref={textInput} defaultValue='hi'/>
+        <p ref={textOut}></p>
+        <h3>{output}</h3>
+      </section>
+    </>
   );
 }
 
